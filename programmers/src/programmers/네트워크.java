0cc5380333f979bff86ffea5f8cd282ac5482606
yaliny <mae1217@naver.com>
2022-08-14 18:58:25 +0900

    네트워크 문제 푼 코드 수정

--- programmers/src/programmers/네트워크.java
+++ programmers/src/programmers/네트워크.java
@@ -1,7 +1,5 @@
 package programmers;
 
-import java.util.Stack;
-
 public class 네트워크 {
 
     public static void main(String[] args) {
@@ -9,19 +7,44 @@ public class 네트워크 {
         int n = 3;
 
         int[][] computers = {
-                {1,1,0},
-                {1,1,1},
-                {0,1,1}
+                {1, 1, 0},
+                {1, 1, 1},
+                {0, 1, 1}
         };
 
+        Sol17 s = new Sol17();
+        System.out.println(s.solution(n, computers));
     }
 
+}
+
+class Sol17 {
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(computers, i, visited);
+                answer++;
+            }
+        }
 
         return answer;
     }
 
+    boolean[] dfs(int[][] computers, int i, boolean[] visited) {
+        visited[i] = true;
+
+        for (int j = 0; j < computers.length; j++) {
+            if (i != j && computers[i][j] == 1 && visited[j] == false) {
+                visited = dfs(computers, j, visited);
+            }
+        }
+
+        return visited;
+    }
 
 }
+
