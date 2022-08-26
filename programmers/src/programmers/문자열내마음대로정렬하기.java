88fd7ce8a74570acb400b8cf8be5929832617a5c
yaliny <mae1217@naver.com>
2022-08-26 10:43:04 +0900

    프로그래머스 문자열 내 마음대로 정렬하기, 나누어 떨어지는 숫자 배열, 키패드 누르기 문제 푼 코드

--- programmers/src/programmers/문자열내마음대로정렬하기.java
+++ programmers/src/programmers/문자열내마음대로정렬하기.java
@@ -9,18 +9,17 @@ public class 문자열내마음대로정렬하기 {
     public static void main(String[] args) {
         // String[] strings = {"sun", "bed", "car"};
         // int n = 1;
-        String[] strings = {"abce", "abcd", "cdx"};
+        // String[] strings = {"abce", "abcd", "cdc"};
+        String[] strings = {"zxc", "abc", "bbc", "fsc", "adcpo"};
         int n = 2;
 
         S26 s = new S26();
         String[] answer = s.solution(strings, n);
         System.out.println(Arrays.toString(answer));
     }
-
 }
 
-class S26{
-    // https://yzink.tistory.com/132
+/*class S26{
     public String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
 
@@ -38,4 +37,78 @@ class S26{
 
         return answer;
     }
+}*/
+
+class S26 {
+    /*public String[] solution(String[] strings, int n) {
+
+        String[] answer = new String[strings.length];
+        ArrayList <String> list = new ArrayList<>();
+        boolean chk;
+        list.add(strings[0]);
+
+        for(int i = 1; i < strings.length; i++){
+            chk = true;
+            for(int j = 0; j < i; j++){
+                if( strings[i].substring(n, n+1).compareTo(strings[j].substring(n, n+1)) <= 0 ){
+                    if( strings[i].substring(n, n+1).equals(strings[j].substring(n, n+1)) ){
+                        list.add(strings[i].compareTo(strings[j]) < 0? j : j+1,strings[i]);
+                        System.out.println(list);
+                    }else{
+                        list.add(j, strings[i]);
+                    }
+                    chk = false;
+                    break;
+                }
+            }
+            if( chk ){
+                list.add(strings[i]);
+                System.out.println(list);
+            }
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }*/
+
+    public String[] solution(String[] strings, int n) {
+
+        String[] answer = new String[strings.length];
+        ArrayList <String> list = new ArrayList<>();
+        boolean chk;
+        list.add(strings[0]);
+
+        for(int i = 1; i < strings.length; i++){
+            chk = true;
+            for(int j = 0; j < i; j++){
+                if( strings[i].substring(n, n+1).compareTo(strings[j].substring(n, n+1)) < 0 ){
+                    list.add(j, strings[i]);
+                    chk = false;
+                    break;
+                } else if( strings[i].substring(n, n+1).equals(strings[j].substring(n, n+1)) ){
+                    if( strings[i].compareTo(strings[j]) > 0 ) continue;
+                    else{
+                        list.add(j, strings[i]);
+                        chk = false;
+                        break;
+                    }
+                }
+            }
+
+            if( chk ){
+                list.add(strings[i]);
+            }
+
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
 }
