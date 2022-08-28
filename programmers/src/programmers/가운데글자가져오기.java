d6e2b167fbedd463e1bfd5a57c08581902e69735
yaliny <mae1217@naver.com>
2022-08-28 22:07:48 +0900

    프로그래머스 음양더하기, 콜라츠추측, 가운데글자가져오기 문제 푼 코드

---
+++ programmers/src/programmers/가운데글자가져오기.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class 가운데글자가져오기 {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length() % 2 == 0) {
+            answer = s.substring(s.length()/2-1, s.length()/2+1);
+        }else{
+            answer = s.charAt(s.length()/2)+"";
+        }
+        return answer;
+    }
+}
