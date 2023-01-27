67fed64e87b8a54caf9ff94227ba0c82ef142a9e
ylpark <ylpark@jcone.co.kr>
2023-01-27 10:39:07 +0900

    프로그래머스 세균증식 문제 푼 코드

---
+++ programmers/src/programmers/세균증식_20230127.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class 세균증식_20230127 {
+    class Solution {
+        public int solution(int n, int t) {
+            int answer = 0;
+            for(int i = 1; i <= t; i++) {
+                n *= 2;
+            }
+            return answer = n;
+        }
+    }
+}
