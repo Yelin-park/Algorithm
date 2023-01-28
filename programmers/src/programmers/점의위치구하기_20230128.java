5eaab2d6b087745d55ece2e401b61d43a9149a0f
yaliny <mae1217@naver.com>
2023-01-28 14:07:50 +0900

    프로그래머스 점의 위치 구하기 문제 푼 코드

---
+++ programmers/src/programmers/점의위치구하기_20230128.java
@@ -0,0 +1,30 @@
+package programmers;
+
+public class 점의위치구하기_20230128 {
+
+    class Solution {
+        public int solution(int[] dot) {
+            int answer = 0;
+            // 1사분면
+            if(dot[0] > 0 && dot[1] > 0 ) {
+                answer = 1;
+
+                // 2사분면
+            } else if(dot[0] < 0 && dot[1] > 0 ) {
+                answer = 2;
+
+                // 3사분면
+            } else if(dot[0] < 0 && dot[1] < 0 ) {
+                answer = 3;
+
+                // 4사분면
+            } else if(dot[0] > 0 && dot[1] < 0 ) {
+                answer = 4;
+
+            }
+
+            return answer;
+        }
+    }
+
+}
