dafd4dd436d46df6b653bd6358a1d0da1e393764
ylpark <ylpark@jcone.co.kr>
2023-01-30 14:15:40 +0900

    프로그래머스 분수의 덧셈, 삼각형의 완성조건, 아이스아메리카노, 특정문자 제거하기 문제 푼 코드

---
+++ programmers/src/programmers/분수의덧셈_20230130.java
@@ -0,0 +1,31 @@
+package programmers;
+
+/*
+    최대 공약수를 유클리드 호제법을 이용해서 구하기
+    임의의 두 자연수 a, b(a>b)가 주어지고 a를 b로 나눈 나머지를 r(r = a % b)라고 하면
+    a = b가되고 b = r이 되는데, 이때 b가 0이 될 때의 a가 최대공약수이다.
+ */
+public class 분수의덧셈_20230130 {
+    class Solution {
+        public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+            int[] answer = new int[2];
+            answer[0] = numer1 * denom2 + numer2 * denom1; // a
+            answer[1] = denom1 * denom2; // b
+
+            int a = answer[0];
+            int b = answer[1];
+            int r = 0;
+
+            while(b != 0 ) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+
+            answer[0] = answer[0] / a;
+            answer[1] = answer[1] / a;
+
+            return answer;
+        }
+    }
+}
