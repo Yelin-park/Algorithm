fb48b34dcbca690b00dfa802b0334da2bf2740a6
Yelin-park <mae1217@naver.com>
2023-02-01 14:37:20 +0900

    프로그래머스 로그인 성공, 숨어있는 숫자의 덧셈 문제 푼 코드

---
+++ programmers/src/programmers/숨어있는숫자의덧셈_20230201.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class 숨어있는숫자의덧셈_20230201 {
+    public static void main(String[] args) {
+        String my_string = "aAb1B2cC34oOp";
+        int answer = 0;
+        String[] numArr = {"1","2","3","4","5","6","7","8","9"};
+
+        for(int i = 0; i < numArr.length; i++){
+            for(int j = 0; j < my_string.length(); j++){
+                if(my_string.substring(j, j+1).equals(numArr[i])) {
+                    answer += Integer.parseInt(numArr[i]);
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+        // return answer;
+    }
+}
