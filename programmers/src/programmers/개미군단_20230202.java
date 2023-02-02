16f7f7fee4cbc06db2832d1fa36e03fd90bbef0e
Yelin-park <mae1217@naver.com>
2023-02-02 15:21:36 +0900

    프로그래머스 모음 제거, 개미군단 문제 푼 코드

---
+++ programmers/src/programmers/개미군단_20230202.java
@@ -0,0 +1,30 @@
+package programmers;
+
+public class 개미군단_20230202 {
+    class Solution {
+        /*
+            개미의 공격력을 나눈 몫을 answer에 추가 나머지를 hp에 할당하는 방법도 있음
+         */
+        public int solution(int hp) {
+            int answer = 0;
+
+            while(true) {
+                if(hp >= 5) {
+                    hp -= 5;
+                    answer++;
+                } else if(hp >= 3) {
+                    hp -= 3;
+                    answer++;
+                } else if(hp >= 1) {
+                    hp -= 1;
+                    answer++;
+                }
+
+                if(hp == 0) {
+                    return answer;
+                }
+            }
+
+        }
+    }
+}
