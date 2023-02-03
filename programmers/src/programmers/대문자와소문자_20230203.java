9dadc15151d870386c59ee6a1096ce5cec26b410
Yelin-park <mae1217@naver.com>
2023-02-03 15:41:46 +0900

    프로그래머스 가위바위보, 대문자와 소문자, 암호해독 문제 푼 코드

---
+++ programmers/src/programmers/대문자와소문자_20230203.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class 대문자와소문자_20230203 {
+    class Solution {
+        public String solution(String my_string) {
+            String answer = "";
+
+            for(int i = 0; i < my_string.length(); i++){
+                if(my_string.charAt(i) >= 65 && my_string.charAt(i) <= 90) {
+                    answer += (my_string.charAt(i)+"").toLowerCase();
+                } else {
+                    answer += (my_string.charAt(i)+"").toUpperCase();
+                }
+            }
+
+            return answer;
+        }
+    }
+}
