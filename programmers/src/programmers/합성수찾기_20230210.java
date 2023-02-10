30ffea28e5b9f93e5e72dad132912e2cf144c827
Yelin-park <mae1217@naver.com>
2023-02-10 09:35:31 +0900

    프로그래머스 문자열 정렬하기(2), 합성수 찾기 문제 푼 코드

---
+++ programmers/src/programmers/합성수찾기_20230210.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class 합성수찾기_20230210 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            int temp = 0;
+
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= i; j++){
+                    if(i % j == 0) temp ++;
+                }
+                if(temp >= 3) answer++;
+                temp = 0;
+            }
+
+            return answer;
+        }
+    }
+}
