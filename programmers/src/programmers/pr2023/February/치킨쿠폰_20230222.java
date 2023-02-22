ec33c326edeed3175a94778b56760a7d08a0d0e6
Yelin-park <mae1217@naver.com>
2023-02-22 17:15:05 +0900

    프로그래머스 치킨 쿠폰 푼 코드

--- programmers/src/programmers/pr2023/February/치킨쿠폰_20230222.java
+++ programmers/src/programmers/pr2023/February/치킨쿠폰_20230222.java
@@ -1,46 +1,19 @@
 package programmers.pr2023.February;
 
 public class 치킨쿠폰_20230222 {
-    public static void main(String[] args) {
-        System.out.println(1081 / 10); // 108
-        System.out.println(1081 % 10); // 1
-        System.out.println((1081 / 10) / 10); // 10
-        System.out.println((1081 % 10) % 10); // 1
-        System.out.println(((1081 / 10) / 10) / 10); // 1
-        System.out.println(((1081 % 10) % 10) % 10); // 1
-        System.out.println((((1081 / 10) / 10) / 10) / 10); // 0
-        System.out.println((((1081 % 10) % 10) % 10) % 10); // 1
-
-        System.out.println("======================================");
-        System.out.println(100 / 10); // 10
-        System.out.println(100 % 10); // 0
-        System.out.println((100 / 10) / 10); // 1
-        System.out.println((100 % 10) % 10); // 0
-        System.out.println(((100 / 10) / 10) / 10); // 0
-        System.out.println(((100 % 10) % 10) % 10); // 0
-
-
-        Solution s = new Solution();
-        int answer = s.solution(1081);
-        System.out.println("answer = " + answer);
-
-    }
-
-    static class Solution {
+    class Solution {
         public int solution(int chicken) {
             int answer = 0;
-            int count = 0;
+            int count = 0; // 치킨 서비스 갯수
             int coupon = chicken;
 
-            while (coupon >= 10) {
-                int leftCoupon = coupon % 10;
-                count += coupon / 10;
-                coupon = leftCoupon + coupon / 10;
+            while(coupon >= 10) {
+                int tempCoupon = coupon % 10; // 남는 쿠폰수
+                count += coupon / 10; // 치킨 서비스 갯수
+                coupon = tempCoupon + coupon / 10;
             }
 
             return answer = count;
-
         }
-
     }
 }
