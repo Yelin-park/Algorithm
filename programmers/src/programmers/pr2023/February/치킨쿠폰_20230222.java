50e52f8ab91260dc3144184268e2305f4c591217
Yelin-park <mae1217@naver.com>
2023-02-23 10:33:08 +0900

    프로그래머스 직사각형 넓이 구하기 푼 코드

--- programmers/src/programmers/pr2023/February/치킨쿠폰_20230222.java
+++ programmers/src/programmers/pr2023/February/치킨쿠폰_20230222.java
@@ -10,7 +10,7 @@ public class 치킨쿠폰_20230222 {
             while(coupon >= 10) {
                 int tempCoupon = coupon % 10; // 남는 쿠폰수
                 count += coupon / 10; // 치킨 서비스 갯수
-                coupon = tempCoupon + coupon / 10;
+                coupon = tempCoupon + coupon / 10; // 서비스 치킨으로 받는 쿠폰 갯수
             }
 
             return answer = count;
