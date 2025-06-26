6299665bc300ac9a3f58f34f52825e245d8ada56
Yaliny <mae1217@naver.com>
2025-06-26 12:57:46 +0900

    feat(백준): 윤년_2753 풀이 코드

--- baekjoon/src/bj2025/AminusB_1001_250625.java
+++ baekjoon/src/bj2025/AminusB_1001_250625.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class AminusB_1001 {
+public class AminusB_1001_250625 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
