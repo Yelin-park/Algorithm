0da749d84c8ea2954258130692f7190fce647d73
Yaliny <mae1217@naver.com>
2025-06-27 15:59:26 +0900

    feat(백준): 아스키코드_11654 풀이 코드

---
+++ baekjoon/src/bj2025/아스키코드_11654_250627.java
@@ -0,0 +1,12 @@
+package bj2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 아스키코드_11654_250627 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int) br.readLine().charAt(0));
+    }
+}
