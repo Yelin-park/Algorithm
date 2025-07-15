1c8a7b104c1e21bc8c49f2635486414eb243834d
Yaliny <mae1217@naver.com>
2025-07-15 19:49:04 +0900

    feat(백준): 피보나치수5_10870 풀이 코드

--- baekjoon/src/bj2025/피보나치수5_10870_250715.java
+++ baekjoon/src/bj2025/피보나치수5_10870_250715.java
@@ -9,6 +9,7 @@ public class 피보나치수5_10870_250715 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        fibonacci(n);
         // 동적 계획법
         int[] dp = new int[n + 1];
         dp[0] = 0;
@@ -26,5 +27,22 @@ public class 피보나치수5_10870_250715 {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    // 공간 복잡도를 O(1)로 줄인 동적 계획법을 활용한 코드
+    public static int fiboFast(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev = 0;  // F(0)
+        int curr = 1;  // F(1)
+
+        for (int i = 2; i <= n; i++) {
+            int next = prev + curr;  // F(i) = F(i-1) + F(i-2)
+            prev = curr;             // prev를 한 칸 앞으로
+            curr = next;             // curr를 한 칸 앞으로
+        }
+
+        return curr;
     }
 }
