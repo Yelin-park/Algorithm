8f0f2b9cdfbe6f0621730abc87c5b4b616f6f132
Yaliny <mae1217@naver.com>
2025-08-11 14:09:24 +0900

    feat(leetCode): 70.ClimbingStairs 풀이 코드

---
+++ leetcode/ClimbingStairs/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package leetcode.ClimbingStairs;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int climbStairs_v2(int n) {
+        if (n <= 2) return n;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
