446332502c71317f0af928c460a1fe7393a0ee76
야리니 <73098948+Yelin-park@users.noreply.github.com>
2025-08-18 12:53:45 +0900

    [Silver V] Title: CD, Time: 920 ms, Memory: 304208 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,37 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while(true) {
-            String s = br.readLine();
-            if (s.equals("0 0")) break;
-            StringTokenizer st = new StringTokenizer(s);
+        StringTokenizer st;
 
+        while (true) {
+            st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            HashMap<Long, Integer> map = new HashMap<>();
-            for (int i = 0; i < N; i++) {
-                long key = Long.parseLong(br.readLine());
-                map.put(key, 1);
-            }
+            if (N == 0 && M == 0) break;
 
+            int[] sang = new int[N];
+            int[] sun = new int[M];
+
+            for (int i = 0; i < N; i++) {
+                sang[i] = Integer.parseInt(br.readLine());
+            }
             for (int i = 0; i < M; i++) {
-                long key = Long.parseLong(br.readLine());
-                map.put(key, map.getOrDefault(key, 0) + 1);
+                sun[i] = Integer.parseInt(br.readLine());
             }
 
             int count = 0;
-            for (int num : map.values()) {
-                if (num == 2) count++;
+            int p1 = 0, p2 = 0;
+
+            while (p1 < N && p2 < M) {
+                if (sang[p1] == sun[p2]) {
+                    count++;
+                    p1++;
+                    p2++;
+                } else if (sang[p1] < sun[p2]) {
+                    p1++;
+                } else {
+                    p2++;
+                }
             }
-            sb.append(count).append("\n");
+
+            System.out.println(count);
         }
-        
-        System.out.println(sb);
     }
 }
