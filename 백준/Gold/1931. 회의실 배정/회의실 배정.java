1a140ae4aa63e6c5b346c0b587f2313fb6c605a6
야리니 <73098948+Yelin-park@users.noreply.github.com>
2025-10-05 17:11:51 +0900

    [Gold V] Title: 회의실 배정, Time: 520 ms, Memory: 46904 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] != b[1]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int count = 0;
+        int lastEnd = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] >= lastEnd) {
+                count++;
+                lastEnd = arr[i][1];
+            }
+        }
+        System.out.println(count);
+    }
+}
